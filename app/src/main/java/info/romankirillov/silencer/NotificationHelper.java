216a06c4bef29eb4e146e18a9b390214e7550942
Roman Kirillov <me@romankirillov.info>
2016-04-14 16:51:23 +0100

    Toggling silence mode properly

--- app/src/main/java/info/romankirillov/silencer/NotificationHelper.java
+++ app/src/main/java/info/romankirillov/silencer/NotificationHelper.java
@@ -25,7 +25,7 @@ public class NotificationHelper {
 
     static void createOrUpdateNotification(Context context) {
         NotificationCompat.Builder notification = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.ic_silent)
+                .setSmallIcon(R.drawable.ic_notification_icon)
                 .setContentTitle(context.getString(R.string.notification_title))
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setOngoing(true);
@@ -54,7 +54,7 @@ public class NotificationHelper {
                 makeSnoozeIntent(context, SNOOZE_15));
         rv.setOnClickPendingIntent(
                 R.id.notification_icon,
-                makeSnoozeIntent(context, iconClickSnoozePeriod));
+                makeToggleSoundIntent(context, currentRingerMode));
 
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -91,6 +91,34 @@ public class NotificationHelper {
         } else {
             return AudioManager.RINGER_MODE_VIBRATE;
         }
+    }
+
+    private static PendingIntent makeToggleSoundIntent(Context context, int currentRingerMode) {
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        intent.putExtra(SNOOZE_FOR, SNOOZE_INDEF);
+
+        int desiredSilentMode = AudioManager.RINGER_MODE_NORMAL;
+
+        switch (currentRingerMode) {
+            case AudioManager.RINGER_MODE_NORMAL:
+                desiredSilentMode = AudioManager.RINGER_MODE_VIBRATE;
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                desiredSilentMode = AudioManager.RINGER_MODE_SILENT;
+                break;
+            case AudioManager.RINGER_MODE_SILENT:
+                desiredSilentMode = AudioManager.RINGER_MODE_NORMAL;
+                break;
+        }
+
+        intent.putExtra(DESIRED_SILENT_MODE, desiredSilentMode);
+        intent.setAction(Integer.toString(SNOOZE_INDEF));
+
+        return PendingIntent.getBroadcast(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     private static PendingIntent makeSnoozeIntent(Context context, int snoozeFor) {
