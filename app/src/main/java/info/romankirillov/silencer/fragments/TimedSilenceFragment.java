3307a1f85d6840205de8f372ebd68a6ca220b3d7
Roman Kirillov <me@romankirillov.info>
2016-04-18 21:48:31 +0100

    Small UI fixes

--- app/src/main/java/info/romankirillov/silencer/fragments/TimedSilenceFragment.java
+++ app/src/main/java/info/romankirillov/silencer/fragments/TimedSilenceFragment.java
@@ -7,8 +7,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 import info.romankirillov.silencer.R;
 import info.romankirillov.silencer.Silencer;
