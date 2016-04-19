c21175d03782d9a680b877ab6a3eb67f9da929ca
Roman Kirillov <me@romankirillov.info>
2016-04-19 21:20:51 +0100

    Silencer fragment is now done, with the new UX

--- app/src/main/java/info/romankirillov/silencer/fragments/TimedSilenceFragment.java
+++ app/src/main/java/info/romankirillov/silencer/fragments/TimedSilenceFragment.java
@@ -1,12 +1,16 @@
 package info.romankirillov.silencer.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
@@ -15,12 +19,18 @@ import android.widget.Spinner;
 import info.romankirillov.silencer.R;
 import info.romankirillov.silencer.Silencer;
 
-public class TimedSilenceFragment extends Fragment implements NumberPicker.OnValueChangeListener, View.OnClickListener {
+public class TimedSilenceFragment extends Fragment implements
+        NumberPicker.OnValueChangeListener,
+        View.OnClickListener,
+        AdapterView.OnItemSelectedListener {
     private NumberPicker numberPickerHours;
     private NumberPicker numberPickerMinutes;
     private ImageView buttonStart;
 
     private int selectedSilenceMinutes = 0;
+    private Spinner spinner;
+
+    private static final String TAG = "TimedSilenceFragment";
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -29,6 +39,7 @@ public class TimedSilenceFragment extends Fragment implements NumberPicker.OnVal
         numberPickerHours = (NumberPicker) getActivity().findViewById(R.id.number_picker_hours);
         numberPickerMinutes = (NumberPicker) getActivity().findViewById(R.id.number_picker_minutes);
         buttonStart = (ImageView) getActivity().findViewById(R.id.timed_silence_start);
+        spinner = (Spinner) getActivity().findViewById(R.id.mode_spinner);
 
         numberPickerHours.setMinValue(0);
         numberPickerHours.setMaxValue(24);
@@ -41,6 +52,19 @@ public class TimedSilenceFragment extends Fragment implements NumberPicker.OnVal
         buttonStart.setOnClickListener(this);
 
         numberPickerMinutes.setValue(15);
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(
+                this.getActivity(),
+                R.layout.mode_spinner_element,
+                new CharSequence[]{
+                        getString(R.string.start_vibro_mode),
+                        getString(R.string.start_silent_mode)});
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        long id = getSharedPreferences().getLong(getString(R.string.timed_silence_default_mode), 0);
+        spinner.setSelection((int) id);
+        spinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -68,6 +92,26 @@ public class TimedSilenceFragment extends Fragment implements NumberPicker.OnVal
         new Silencer(
                 this.getActivity().getApplicationContext(),
                 selectedSilenceMinutes * 60,
-                AudioManager.RINGER_MODE_VIBRATE).silence();
+                spinner.getSelectedItemId() == 0
+                        ? AudioManager.RINGER_MODE_VIBRATE : AudioManager.RINGER_MODE_SILENT)
+                .silence();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        SharedPreferences.Editor editor = getSharedPreferences().edit();
+        editor.putLong(getString(R.string.timed_silence_default_mode), id);
+        Log.d(TAG, "Selected row: " + id);
+        editor.commit();
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        return getActivity().getSharedPreferences(
+                getString(R.string.default_preferences_name),
+                Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
