bb4894d87066a38d7bb21ce7f99dffbc0e600689
Roman Kirillov <me@romankirillov.info>
2016-04-22 23:05:02 +0100

    New silencer layout ready

---
+++ app/src/main/java/info/romankirillov/silencer/SilencerFragment.java
@@ -0,0 +1,35 @@
+package info.romankirillov.silencer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+
+public class SilencerFragment extends Fragment {
+    private Spinner modeSpinner;
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.silencer_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.modeSpinner = (Spinner) getActivity().findViewById(R.id.silenceModeSpinner);
+
+    }
+}
