572b69f541668e6a99518c9303ffa970f749a4a9
Roman Kirillov <me@romankirillov.info>
2016-04-24 13:57:45 +0100

    Playing with layouts

--- app/src/main/java/info/romankirillov/silencer/SilenceFragment.java
+++ app/src/main/java/info/romankirillov/silencer/SilenceFragment.java
@@ -1,9 +1,24 @@
 package info.romankirillov.silencer;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by kirillov on 22/04/2016.
  */
 public class SilenceFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.silencer_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
