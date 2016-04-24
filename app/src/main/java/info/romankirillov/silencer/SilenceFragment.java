12077ef3a7cbc3f6030dcd103499e955e771dbbe
Roman Kirillov <me@romankirillov.info>
2016-04-24 14:17:09 +0100

    Mode spinner in silencer fragment

--- app/src/main/java/info/romankirillov/silencer/SilenceFragment.java
+++ app/src/main/java/info/romankirillov/silencer/SilenceFragment.java
@@ -6,15 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * Created by kirillov on 22/04/2016.
- */
 public class SilenceFragment extends Fragment {
+    private Spinner modeSpinner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.silencer_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        modeSpinner = (Spinner) getActivity().findViewById(R.id.silenceModeSpinner);
+        ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(
+                this.getActivity(),
+                R.layout.silence_mode_spinner_item,
+                new String[] {
+                        getString(R.string.vibro_mode),
+                        getString(R.string.silent_mode),
+                });
+        modeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        modeSpinner.setAdapter(modeAdapter);
     }
 
     @Override
