c401c9790dc9760cd7a9f6837385594f48bc3592
Roman Kirillov <me@romankirillov.info>
2016-04-25 17:06:12 +0100

    Custom picker is now working

--- app/src/main/java/info/romankirillov/silencer/SilenceFragment.java
+++ app/src/main/java/info/romankirillov/silencer/SilenceFragment.java
@@ -1,27 +1,41 @@
 package info.romankirillov.silencer;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class SilenceFragment extends Fragment {
+public class SilenceFragment extends Fragment implements TimePickerDialog.OnTimeSetListener {
     private static final String MODE_PREFERENCE_KEY =
             SilenceFragment.class.getCanonicalName() + ".mode";
     private static final String DURATION_PREFERENCE_KEY =
             SilenceFragment.class.getCanonicalName() + ".duration";
+    private static final String CUSTOM_DURATION_PREFERENCE_KEY =
+            SilenceFragment.class.getCanonicalName() + ".custom_durtion";
+    private static final String CUSTOM_DURATION_HOURS_PREF_KEY =
+            SilenceFragment.class.getCanonicalName() + ".custom_durtion.hours";
+    private static final String CUSTOM_DURATION_MINUTES_PREF_KEY =
+            SilenceFragment.class.getCanonicalName() + ".custom_durtion.minutes";
+
     public static final String PREF_KEY = SilenceFragment.class.getCanonicalName();
 
     private Spinner modeSpinner;
     private Spinner durationSpinner;
     private SharedPreferences preferences;
+    private TextView customDurationText;
+    private int durationHours = 0;
+    private int durationMinutes = 0;
 
     @Nullable
     @Override
@@ -35,6 +49,7 @@ public class SilenceFragment extends Fragment {
 
         modeSpinner = (Spinner) getActivity().findViewById(R.id.silenceModeSpinner);
         durationSpinner = (Spinner) getActivity().findViewById(R.id.durationSpinner);
+        customDurationText = (TextView) getActivity().findViewById(R.id.customDurationText);
 
         ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(
                 this.getActivity(),
@@ -56,13 +71,7 @@ public class SilenceFragment extends Fragment {
                         getString(R.string.min_75),
                         getString(R.string.min_90),
                         getString(R.string.min_120),
-                        getString(R.string.min_150),
-                        getString(R.string.min_180),
-                        getString(R.string.min_240),
-                        getString(R.string.min_300),
-                        getString(R.string.min_360),
-                        getString(R.string.min_420),
-                        getString(R.string.min_480),
+                        getString(R.string.min_custom)
                 });
 
         durationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -72,6 +81,9 @@ public class SilenceFragment extends Fragment {
 
         modeSpinner.setSelection(preferences.getInt(MODE_PREFERENCE_KEY, 0));
         durationSpinner.setSelection(preferences.getInt(DURATION_PREFERENCE_KEY, 0));
+
+        durationHours = preferences.getInt(CUSTOM_DURATION_HOURS_PREF_KEY, 0);
+        durationMinutes = preferences.getInt(CUSTOM_DURATION_MINUTES_PREF_KEY, 0);
 
         modeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -93,15 +105,55 @@ public class SilenceFragment extends Fragment {
                         .edit()
                         .putInt(DURATION_PREFERENCE_KEY, position)
                         .commit();
+
+                if (position == durationAdapter.getCount() - 1) {
+                    // Last element is Custom
+                    preferences.edit().putBoolean(CUSTOM_DURATION_PREFERENCE_KEY, true).commit();
+                    customDurationText.setVisibility(View.VISIBLE);
+                    startTimePickerDialogue();
+                } else {
+                    preferences.edit().putBoolean(CUSTOM_DURATION_PREFERENCE_KEY, false).commit();
+                    customDurationText.setVisibility(View.GONE);
+                }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
+
+        durationSpinner.setGravity(Gravity.CENTER_HORIZONTAL);
+    }
+
+    private void startTimePickerDialogue() {
+        new TimePickerDialog(
+                this.getActivity(),
+                this,
+                durationHours,
+                durationMinutes,
+                true).show();
+    }
+
+    private void redrawCustomText() {
+        // TODO: Localize me
+        customDurationText.setText(
+                durationHours + " hours " + durationMinutes + " minutes");
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        durationHours = hourOfDay;
+        durationMinutes = minute;
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(CUSTOM_DURATION_HOURS_PREF_KEY, durationHours);
+        editor.putInt(CUSTOM_DURATION_MINUTES_PREF_KEY, durationMinutes);
+        editor.commit();
+
+        redrawCustomText();
     }
 }
